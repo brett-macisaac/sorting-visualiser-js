f830f20a42d27679b02b298f4119bbcd2b351dba
Brett MacIsaac <brett.macisaac@outlook.com>
2022-11-19 17:15:31 +1100

    Implemented QuickSort and added shuffle function.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 
-console.log("Hello world!");
-
-
 /*
 * This function is called when the browser loads the window.
 * It performs initial actions that must be made when the web app runs.
 */
 function init()
 {
-    console.log("Hello world!");
-    CreateElements(50);
+    document.getElementById("btnSort").onclick = SortElements;
+    document.getElementById("btnShuffle").onclick = ShuffleElements;
+
+    CreateElements(100);
 }
-//window.onload = init; // Call init when the browser loads the window.
-init();
+window.onload = init; // Call init when the browser loads the window.
+//init();
 
 
 // Auxiliaries (X) =====================================================================================================
@@ -50,9 +49,9 @@ function CreateElements(aNumElements)
     // https://stackoverflow.com/questions/67914015/javascript-shows-empty-style-property-for-document-objects-that-i-have-styled-us
     const lElementsStyle = window.getComputedStyle(lElements);
 
-    console.log(`Container width: ${lElementsStyle.width}.`);
-    console.log(`Container height: ${lElementsStyle.height}.`);
-    console.log(lElements);
+    // console.log(`Container width: ${lElementsStyle.width}.`);
+    // console.log(`Container height: ${lElementsStyle.height}.`);
+    // console.log(lElements);
 
     const lElementsWidth = Number(lElementsStyle.width.slice(0, lElementsStyle.width.length - 2));
 
@@ -61,10 +60,13 @@ function CreateElements(aNumElements)
     // The width of each element (integer value i.e. pixels).
     const lElementWidth = Math.floor(lElementsWidth / aNumElements);
 
-    // Change lElements width so that it fits its elements exactly.
-    lElements.style.width = `${lElementWidth * aNumElements}px`;
+    const lPaddingSides = 2 * Number(lElementsStyle.paddingLeft.slice(0, lElementsStyle.paddingLeft.length - 2));
 
-    console.log(`Element width: ${lElementWidth}.`);
+    // Change lElements width so that it fits its elements exactly (make sure to update the left margin to re-centre it).
+    lElements.style.width = `${lElementWidth * aNumElements}px`;
+    lElements.style.marginLeft = `-${(lElementWidth * aNumElements + lPaddingSides) / 2}px`;
+
+    // console.log(`Element width: ${lElementWidth}.`);
 
     // Add the elements to lElements.
     for (let i = 0; i < aNumElements; ++i)
@@ -90,7 +92,49 @@ function CreateElements(aNumElements)
 
 }
 
-/*
+/* Auxiliary of init
+* Begins the sorting process.
+*/
+async function SortElements()
+{
+    document.getElementById("btnSort").disabled = true;
+    document.getElementById("btnShuffle").disabled = true;
+
+    const lElements = document.getElementsByClassName("element");
+
+    for (let lIndexUnsortedUpper = lElements.length - 1; lIndexUnsortedUpper > 0; --lIndexUnsortedUpper)
+    {
+        for (let i = 1; i <= lIndexUnsortedUpper; ++i)
+        {
+            if (lElements[i - 1].clientHeight > lElements[i].clientHeight)
+            {
+                SwapElements(lElements[i - 1], lElements[i]);
+                await sleep(1);
+            }
+        }
+    }
+
+    document.getElementById("btnSort").disabled = false;
+    document.getElementById("btnShuffle").disabled = false;
+}
+
+/* Auxiliary of init
+* Shuffles the elements.
+*/
+function ShuffleElements()
+{
+    const lElements = document.getElementsByClassName("element");
+
+    for (let i = lElements.length - 1; i > 0; --i)
+    {
+        const lIndexRandom = GetRandom(0, i);
+
+        SwapElements(lElements[i], lElements[lIndexRandom]);
+    }
+
+}
+
+/* Auxiliary of CreateElements, ShuffleElements
 * This function returns a random number between aMin and aMax (inclusive of both, i.e. [aMin, aMax]).
 
 * Parameters:
@@ -100,4 +144,30 @@ function CreateElements(aNumElements)
 function GetRandom(aMin, aMax)
 {
     return Math.floor(Math.random() * (aMax - aMin + 1)) + aMin;
+}
+
+/* Auxiliary of
+* Swaps the heights of the given elements.
+*/
+function SwapElements(aElement1, aElement2)
+{
+    const lHeight1 = aElement1.style.height;
+
+    aElement1.style.height = aElement2.style.height;
+
+    aElement2.style.height = lHeight1;
+}
+
+function sleep(aSleepDuration)
+{
+    if (typeof aSleepDuration !== 'number')
+    {
+        console.log("aSleepDuration must be a number.");
+    } 
+    else if (aSleepDuration < 0)
+    {
+        console.log("aSleepDuration can't be negative.");
+    }
+
+    return new Promise (resolve => setTimeout(resolve, aSleepDuration));
 }
